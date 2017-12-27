5348019a6c87a2506ebb7f82605e8cda8d432e14
subzerodeluxe <maarten.waanders@gmail.com>
2017-12-27 16:02:43 +0100

    L'update majeur

--- website/public/js/script.js
+++ website/public/js/script.js
@@ -1,121 +1,139 @@
-d3.json('js/data/forecast.json', function(d) {
+var margin = {top: (parseInt(d3.select('body').style('width'), 10)/10), right: (parseInt(d3.select('body').style('width'), 10)/20), 
+  bottom: (parseInt(d3.select('body').style('width'), 10)/5), left: (parseInt(d3.select('body').style('width'), 10)/20)},
+  
+  width = parseInt(d3.select('body').style('width'), 10) - margin.left - margin.right,
+  height = parseInt(d3.select('body').style('height'), 10) - margin.top - margin.bottom;
+
+
+var x0 = d3.scale.ordinal()
+  .rangeRoundBands([0, width], .1);
+
+var x1 = d3.scale.ordinal();
+
+var y = d3.scale.linear()
+  .range([height, 0]);
+
+var colorRange = d3.scale.category20();
+var color = d3.scale.ordinal()
+  .range(colorRange.range());
+
+var xAxis = d3.svg.axis()
+  .scale(x0)
+  .orient("bottom");
+
+var yAxis = d3.svg.axis()
+  .scale(y)
+  .orient("left")
+  .tickFormat(d3.format(".2s"));
+
+var divTooltip = d3.select('body').append("div").attr("class", "toolTip");
+
+var svg = d3.select('#chart').append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var fixed_num = parseFloat(3.855422).toFixed( 2 );
+  console.log(fixed_num);
+
+  pcDataset = [
+    {label:"Boys", "Not interested at all":4, "Not interested":8, "Neutral": 24, "Interested":26, "Very Interested":38},
+    {label:"Girls", "Not interested at all":20, "Not interested":29, "Neutral": 27, "Interested":16, "Very Interested":8}
+  ];
+  
+
+var options = d3.keys(pcDataset[0]).filter(function(key) { return key !== "label"; });
+
+pcDataset.forEach(function(d) {
+  d.valores = options.map(function(name) { return {name: name, value: +d[name]}; });
+});
+
+x0.domain(pcDataset.map(function(d) { return d.label; }));
+x1.domain(options).rangeRoundBands([0, x0.rangeBand()]);
+y.domain([0, d3.max(pcDataset, function(d) { return d3.max(d.valores, function(d) { return d.value; }); })]);
+
+svg.append("g")
+  .attr("class", "x axis")
+  .attr("transform", "translate(0," + height + ")")
+  .call(xAxis);
+
+svg.append("g")
+  .attr("class", "y axis")
+  .call(yAxis)
+  .append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 6)
+  .attr("dy", ".71em")
+  .style("text-anchor", "end")
+  .text("Rating %");
+
+var bar = svg.selectAll(".bar")
+  .data(pcDataset)
+  .enter().append("g")
+  .attr("class", "rect")
+  .attr("transform", function(d) { return "translate(" + x0(d.label) + ",0)"; });
+
+bar.selectAll("rect")
+  .data(function(d) { return d.valores; })
+  .enter().append("rect")
+  .attr("width", x1.rangeBand())
+  .attr("x", function(d) { return x1(d.name); })
+  .attr("y", function(d) { return y(d.value); })
+  .attr("value", function(d){return d.name;})
+  .attr("height", function(d) { return height - y(d.value); })
+  .style("fill", function(d) { return color(d.name); });
+
+bar.on("mousemove", function(d){
+    divTooltip.style("left", d3.event.pageX+10+"px");
+    divTooltip.style("top", d3.event.pageY-25+"px");
+    divTooltip.style("display", "inline-block");
+    var x = d3.event.pageX, y = d3.event.pageY
+    var elements = document.querySelectorAll(':hover');
+    l = elements.length
+    l = l-1
+    elementData = elements[l].__data__
+    divTooltip.html((d.label)+"<br>"+elementData.name+"<br>"+elementData.value+"%");
+});
+
+bar.on("mouseout", function(d){
+    divTooltip.style("display", "none");
+});
+
+var legend = svg.selectAll(".legend")
+  .data(options.slice())
+  .enter().append("g")
+  .attr("class", "legend")
+  .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+legend.append("rect")
+  .attr("x", width - 18)
+  .attr("width", 18)
+  .attr("height", 18)
+  .style("fill", color);
+
+legend.append("text")
+  .attr("x", width - 24)
+  .attr("y", 9)
+  .attr("dy", ".35em")
+  .style("text-anchor", "end")
+  .text(function(d) { return d; });
+
+  function removeChart() {
+    var element = document.getElementById("chart");
+    element.parentNode.removeChild(element);
+  }
+
+ 
+
+ 
+/// UPDATE CHART 
+  function updateBars() {	
     
-      var temperatures = [],
-          dates = [],
-          margin = { top: 0, right: 0, bottom: 30, left: 20 }
-          height = 400 - margin.top - margin.bottom,
-          width = 600 - margin.left - margin.right;
-    
-      var   tempColor,
-            yScale,
-            yAxisValues,
-            yAxisTicks,
-            yGuide,
-            xScale,
-            xAxisValues,
-            xAxisTicks,
-            xGuide,
-            colors,
-            tooltip,
-            myChart;
-    
-      for (var i = 0; i<d.list.length; i++) {
-        temperatures.push(d.list[i].main.temp);
-        dates.push( new Date(d.list[i].dt_txt) );
-      }
-    
-      yScale = d3.scaleLinear()
-        .domain([0, d3.max(temperatures)])
-        .range([0,height]);
-    
-      yAxisValues = d3.scaleLinear()
-        .domain([0, d3.max(temperatures)])
-        .range([height,0]);
-    
-      yAxisTicks = d3.axisLeft(yAxisValues)
-      .ticks(10)
-    
-      xScale = d3.scaleBand()
-        .domain(temperatures)
-        .paddingInner(.1)
-        .paddingOuter(.1)
-        .range([0, width])
-    
-      xAxisValues = d3.scaleTime()
-        .domain([dates[0],dates[(dates.length-1)]])
-        .range([0, width])
-    
-      xAxisTicks = d3.axisBottom(xAxisValues)
-        .ticks(d3.timeDay.every(1))
-    
-      colors = d3.scaleLinear()
-        .domain([0, 65, d3.max(temperatures)])
-        .range(['#FFFFFF', '#2D8BCF', '#DA3637'])
-    
-      tooltip = d3.select('body')
-        .append('div')
-        .style('position', 'absolute')
-        .style('padding', '0 10px')
-        .style('background', 'white')
-        .style('opacity', 0);
-    
-      myChart = d3.select('#viz').append('svg')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-        .append('g')
-        .attr('transform',
-          'translate(' + margin.left + ',' + margin.right + ')')
-        .selectAll('rect').data(temperatures)
-        .enter().append('rect')
-          .attr('fill', colors)
-          .attr('width', function(d) {
-            return xScale.bandwidth();
-          })
-          .attr('height', 0)
-          .attr('x', function(d) {
-            return xScale(d);
-          })
-          .attr('y', height)
-          
-          .on('mouseover', function(d) {
-            tooltip.transition().duration(200)
-              .style('opacity', .9)
-            tooltip.html(
-              '<div style="font-size: 2rem; font-weight: bold">' +
-                d + '&deg;</div>'
-            )
-              .style('left', (d3.event.pageX -35) + 'px')
-              .style('top', (d3.event.pageY -30) + 'px')
-            tempColor = this.style.fill;
-            d3.select(this)
-              .style('fill', 'yellow')
-          })
-    
-          .on('mouseout', function(d) {
-            tooltip.html('')
-            d3.select(this)
-              .style('fill', tempColor)
-          });
-    
-      yGuide = d3.select('#viz svg').append('g')
-                .attr('transform', 'translate(20,0)')
-                .call(yAxisTicks)
-    
-      xGuide = d3.select('#viz svg').append('g')
-                .attr('transform', 'translate(20,'+ height + ')')
-                .call(xAxisTicks)
-    
-      myChart.transition()
-        .attr('height', function(d) {
-          return yScale(d);
-        })
-        .attr('y', function(d) {
-          return height - yScale(d);
-        })
-        .delay(function(d, i) {
-          return i * 20;
-        })
-        .duration(1000)
-        .ease(d3.easeBounceOut)
-    
-    }); // json import
+    removeChart(); 
+
+  
+
+  }
+ 
+  d3.select("#change").on("click", updateBars);
